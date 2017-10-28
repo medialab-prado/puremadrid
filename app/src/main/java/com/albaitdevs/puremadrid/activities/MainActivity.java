f67db933a35c3b1857e54d903721c5388e3f5760
Javier Delgado <delga.soria@gmail.com>
2017-10-29 01:53:54 +0200

    Clean

--- app/src/main/java/com/albaitdevs/puremadrid/activities/MainActivity.java
+++ app/src/main/java/com/albaitdevs/puremadrid/activities/MainActivity.java
@@ -155,19 +155,7 @@ public class MainActivity extends Activity implements NavigationView.OnNavigatio
         actionBarDrawerToggle.syncState();
 
         replaceFragment(mCurrentDrawerItem);
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String currentEstado = prefs.getString(NotificationData.currentEstadoName,Escenario.NONE.name());
-        String validEstado = prefs.getString(NotificationData.validEstadoName,Escenario.NONE.name());
-        String maxEstado = prefs.getString(NotificationData.maxEstadoName,Escenario.NONE.name());
-        String escenarioToday = prefs.getString(NotificationData.escenarioNameToday,Escenario.NONE.name());
-        String escenarioTomorrow = prefs.getString(NotificationData.escenarioNameTomorrow,Escenario.NONE.name());
-
-        Escenario esceneToday = Escenario.valueOf(escenarioToday);
-        Escenario esceneTomorrow = Escenario.valueOf(escenarioTomorrow);
-        Estado state = Estado.valueOf(currentEstado);
-
-
+        
     }
 
     @Override
@@ -697,62 +685,5 @@ public class MainActivity extends Activity implements NavigationView.OnNavigatio
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
-
-
-
-
-
-
-
-
-
-
-    /**
-     * EXPERIMENTS ========================================================
-     */
-
-
-
-
-
-//    private void shortcutAdd(String name, int number) {
-//        // Intent to be send, when shortcut is pressed by user ("launched")
-//        Intent shortcutIntent = new Intent(getApplicationContext(), MainActivity.class);
-//        shortcutIntent.setAction("ASDA");
-//
-//        // Create bitmap with number in it -> very default. You probably want to give it a more stylish look
-//        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-//        Paint paint = new Paint();
-//        paint.setColor(0xFF808080); // gray
-//        paint.setTextAlign(Paint.Align.CENTER);
-//        paint.setTextSize(50);
-//        new Canvas(bitmap).drawText(""+number, 50, 50, paint);
-////        ((ImageView) findViewById(android.R.id.icon)).setImageBitmap(bitmap);
-//
-//        // Decorate the shortcut
-//        Intent addIntent = new Intent();
-//        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-//        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-//        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, bitmap);
-//
-//        // Inform launcher to create shortcut
-//        addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-//        getApplicationContext().sendBroadcast(addIntent);
-//    }
-//
-//    private void shortcutDel(String name) {
-//        // Intent to be send, when shortcut is pressed by user ("launched")
-//        Intent shortcutIntent = new Intent(getApplicationContext(), MainActivity.class);
-//        shortcutIntent.setAction("ASDA");
-//
-//        // Decorate the shortcut
-//        Intent delIntent = new Intent();
-//        delIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-//        delIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-//
-//        // Inform launcher to remove shortcut
-//        delIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
-//        getApplicationContext().sendBroadcast(delIntent);
-//    }
 
 }
